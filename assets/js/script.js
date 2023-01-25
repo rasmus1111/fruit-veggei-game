cf9c0e0c15ca26eaf992c3da0cae096c1d7d4708
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-01-25 21:34:28 +0000

    Add two arrays one with fruits one with vegetables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,34 @@
+// Declare two arrays, one for fruits and one for vegetables
+const  fruits = ["Acai", "Durian", "Mangosteen", "Rambutan", "Kiwano", "Gooseberry", "Lychee", "Persimmon", "Pomegranate", "Soursop", "Apple", "Banana", "Cherry", "Grape", "Orange"];
+const  vegetables = ["Artichoke", "Bok Choy", "Celeriac", "Eggplant", "Fennel", "Jicama", "Kohlrabi", "Leeks", "Nopales", "Okra", "Parsnips", "Radicchio", "Scallions", "Turnips", "Carrots","Cauliflower"]
 
 
 
-
+// function to randomly display a fruit or vegetable
 function startGame(){
+    if(Math.random() > 0.5)
+    displayFruit();
+    else
+    displayVegetable();
+  }
 
-}
 
 
-
+// function to display a random fruit from the fruits array
 function displayFruit(){
+    displayedItem = 'fruit'; 
+    var index = Math.floor(Math.random()*fruits.length); 
+    document.getElementById("quiz-question").innerHTML = fruits[index]; 
+  }
+  
+// function to display a random vegetable from the vegetables array
+  function displayVegetable(){
+    displayedItem = 'vegie'; 
+    var index = Math.floor(Math.random()*vegetables.length); 
+    document.getElementById("quiz-question").innerHTML = vegetables[index]; 
+  }
 
-}
-
-
-
-function displayVegetable(){
-
- 
-}
+  
 
 
  function checkVictory(){
@@ -41,12 +52,12 @@ function displayVegetable(){
  }
 
 
-function checkAnswer(item){
+function checkAnswer(){
  
 }
 
 
 
-
+window.onload = startGame;
 
 
