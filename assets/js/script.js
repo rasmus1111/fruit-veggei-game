f75525ff731617a22b3d0fb3142130cc71e3e9d0
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-01-25 22:35:05 +0000

    Add funtion to alert win score and lost score to the player based on the score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,92 @@
 // Declare two arrays, one for fruits and one for vegetables
-const  fruits = ["Acai", "Durian", "Mangosteen", "Rambutan", "Kiwano", "Gooseberry", "Lychee", "Persimmon", "Pomegranate", "Soursop", "Apple", "Banana", "Cherry", "Grape", "Orange"];
-const  vegetables = ["Artichoke", "Bok Choy", "Celeriac", "Eggplant", "Fennel", "Jicama", "Kohlrabi", "Leeks", "Nopales", "Okra", "Parsnips", "Radicchio", "Scallions", "Turnips", "Carrots","Cauliflower"]
+const fruits = ["Acai", "Durian", "Mangosteen", "Rambutan", "Kiwano", "Gooseberry", "Lychee", "Persimmon", "Pomegranate", "Soursop", "Apple", "Banana", "Cherry", "Grape", "Orange"];
+const vegetables = ["Artichoke", "Bok Choy", "Celeriac", "Eggplant", "Fennel", "Jicama", "Kohlrabi", "Leeks", "Nopales", "Okra", "Parsnips", "Radicchio", "Scallions", "Turnips", "Carrots", "Cauliflower"]
 
 let displayedItem;
 let rightScore = 0; // variable to store the number of correct answers
 let wrongScore = 0; // variable to store the number of incorrect answers
 
 // function to randomly display a fruit or vegetable
-function startGame(){
-    if(Math.random() > 0.5)
-    displayFruit();
+function startGame() {
+    if (Math.random() > 0.5)
+        displayFruit();
     else
-    displayVegetable();
-  }
+        displayVegetable();
+}
 
 
 
 //display a random fruit from the fruits array
-function displayFruit(){
-    displayedItem = 'fruit'; 
-    var index = Math.floor(Math.random()*fruits.length); 
-    document.getElementById("quiz-question").innerHTML = fruits[index]; 
-  }
-  
+function displayFruit() {
+    displayedItem = 'fruit';
+    var index = Math.floor(Math.random() * fruits.length);
+    document.getElementById("quiz-question").innerHTML = fruits[index];
+}
+
 //display a random vegetable from the vegetables array
-  function displayVegetable(){
-    displayedItem = 'vegie'; 
-    var index = Math.floor(Math.random()*vegetables.length); 
-    document.getElementById("quiz-question").innerHTML = vegetables[index]; 
-  }
-
-  
-
-
- function checkVictory(){
-   
+function displayVegetable() {
+    displayedItem = 'vegie';
+    var index = Math.floor(Math.random() * vegetables.length);
+    document.getElementById("quiz-question").innerHTML = vegetables[index];
 }
 
 
- function checkLoss(){
-  
+
+// check if the user has won the game by getting 10 right
+function checkVictory() {
+    if (rightScore === 10) {
+        document.getElementById('victory-message').innerHTML = 'YOU WON!';
+        console.log('i am showing because i won')
+        resetScore()
+    }
+}
+
+// check if the user has lost the game by getting 10 wrong
+function checkLoss() {
+    if (wrongScore === 10) {
+        document.getElementById('loss-message').innerHTML = 'YOU LOST!';
+        console.log('losser!')
+        resetScore()
+    }
+}
+
+function resetScore() {
+
+
 }
 
 
- function resetScore(){
-     
-    
+function resetMessage() {
+
 }
-
-
- function resetMessage(){
- 
- }
 
 
 // function to increment the users score
-function checkAnswer(item){
-    if(displayedItem === item){
-      rightScore = rightScore + 1; 
-      document.getElementById("right-answer-score").innerHTML = rightScore;
-    }else{
-      wrongScore = wrongScore + 1;
-      document.getElementById("wrong-answer-score").innerHTML = wrongScore; 
+function checkAnswer(item) {
+    if (displayedItem === item) {
+        rightScore = rightScore + 1;
+        document.getElementById("right-answer-score").innerHTML = rightScore;
+    } else {
+        wrongScore = wrongScore + 1;
+        document.getElementById("wrong-answer-score").innerHTML = wrongScore;
     }
-  
-  
-    startGame(); 
-  }
+
+
+    startGame();
+}
 
 // start the game when the page loads
 window.onload = startGame;
 
-document.getElementById("fruit-button").addEventListener("click", function(){ 
-   checkAnswer("fruit"); 
+document.getElementById("fruit-button").addEventListener("click", function () {
+    checkAnswer("fruit");
+    checkVictory()
+    checkLoss()
 
 })
 
-document.getElementById("vegie-button").addEventListener("click", function(){
+document.getElementById("vegie-button").addEventListener("click", function () {
     checkAnswer("vegie");
+    checkVictory()
+    checkLoss()
 })
-
